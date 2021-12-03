688d49b545a3c100f8c7dd744c6545285ccbd3f8
Mark Braspenning <bras@sandcat.nl>
2021-12-03 11:13:41 +0100

    tldr versie

--- 3/part2.py
+++ 3/part2.py
@@ -1,39 +1,22 @@
-import re
-from collections import Counter
-
 lines = open("input.1").read()
 #lines = open("input.test").read()
-lines = lines.splitlines()
+keys = [list(line) for line in lines.splitlines()]
 
 def column(matrix, i):
     return [row[i] for row in matrix]
 
-keys = []
-
-for line in lines:
-    keys.append( list(line) )
-
 oxygen = keys
-
 for col in range(len(keys[0])):
-    if Counter(column(oxygen, col))["1"] >= Counter(column(oxygen, col))["0"]:
-        # removde all keys with 0  at position col
-        oxygen = [row for row in oxygen if row[col] == "1"]
-    else:
-        oxygen = [row for row in oxygen if row[col] == "0"]
+    val = "1" if column(oxygen, col).count("1") >= column(oxygen, col).count("0") else "0"
+    oxygen = [row for row in oxygen if row[col] == val]
     if len(oxygen) == 1:
         break
 
 co2 = keys
 for col in range(len(keys[0])):
-    if Counter(column(co2, col))["1"] >= Counter(column(co2, col))["0"]:
-        co2 = [row for row in co2 if row[col] == "0"]
-    else:
-        co2 = [row for row in co2 if row[col] == "1"]
+    val = "0" if column(co2, col).count("1") >= column(co2, col).count("0") else "1"
+    co2 = [row for row in co2 if row[col] == val]
     if len(co2) == 1:
         break
-
-print(int(''.join(oxygen[0]), 2))
-print(int(''.join(co2[0]), 2))
 
 print(int(''.join(oxygen[0]), 2) * int(''.join(co2[0]), 2))
