eb804f01be1452a3890345cc657fc6f68198f7fa
Mark Braspenning <bras@sandcat.nl>
2021-12-03 11:03:19 +0100

    tldr versie

--- 3/part1.py
+++ 3/part1.py
@@ -1,27 +1,15 @@
-import re
-from collections import Counter
-
 lines = open("input.1").read()
 #lines = open("input.test").read()
-lines = lines.splitlines()
+keys = [list(line) for line in lines.splitlines()] 
 
 def column(matrix, i):
     return [row[i] for row in matrix]
 
-keys = []
-
-gamma = ""
+gamma   = ""
 epsilon = ""
 
-for line in lines:
-    keys.append( list(line) )
-
 for col in range(len(keys[0])):
-    if Counter(column(keys, col))["1"] > Counter(column(keys, col))["0"]:
-        gamma += "1"
-        epsilon += "0"
-    else:
-        gamma += "0"
-        epsilon += "1"
+    gamma   += "1" if column(keys, col).count("1") > column(keys, col).count("0") else "0"
+    epsilon += "0" if gamma[-1] == "1" else "1"
 
 print(int(gamma,2) * int(epsilon,2))
