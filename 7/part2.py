9929fe40816a8c93eddb5c6f99930227be941e96
Mark Braspenning <bras@sandcat.nl>
2021-12-07 09:03:12 +0100

    use a lookup table for fuelcost, speeding up the calculation by a lot

--- 7/part2.py
+++ 7/part2.py
@@ -1,25 +1,19 @@
 line = open("input.1").read()
-line = open("input.test").read()
+#line = open("input.test").read()
 
 crabs = [int(i) for i in line.split(",")]
 
 # distance cost table
 dc = {} 
 dc[0] = 0
-for i in range(1, max(crabs) - min(crabs)):
+for i in range(1, max(crabs) - min(crabs) + 1):
     dc[i] = dc[i - 1] + i
-
-print(dc)
 
 minfuel = 9999999999999999999999999999999999999
 for i in range(min(crabs), max(crabs) + 1):
     fuel = 0
     for crab in crabs:
-        distance = abs(crab - i)
-        cost = 0
-        for j in range(1, distance + 1):
-            cost += j
-        fuel += cost
+        fuel += dc[abs(crab - i)]
 
     if fuel < minfuel:
         minfuel = fuel
